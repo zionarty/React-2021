844b55576f92d5701451c59cf987d4a77ae93486
maneerat.m <51904844+zionarty@users.noreply.github.com>
2021-03-27 21:56:54 +0700

    JSX

--- myfirstreact/src/index.js
+++ myfirstreact/src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+//Example1-2 Coding JSX
+const myElement = <h1>Test Element</h1>
+const testelement = React.createElement('h1',{},'Test Element2')
+
+//Expressions in JSX
+const testElement2 = <h1>React is {5+5} time better with jsx</h1>
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  testelement2,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
